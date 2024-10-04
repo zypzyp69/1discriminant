bd209cd28f53373c74685fefbd0530a699466696
zypzyp69 <161214966+zypzyp69@users.noreply.github.com>
2024-10-04 23:40:17 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quadratic</title>
+
+</head>
+
+<body>
+
+    <h1>Discriminant of a quadratic equation</h1>
+
+    <form action='testGet.php' method='GET'>
+        <p>A
+            <input type="text" name="a" size="50" maxlength="30" placeholder="Value of a here"/>
+        </p>
+        <p>B
+            <input type="text" name="b" size="50" maxlength="30" placeholder="Value of b here"/>
+        </p>
+        <p>A
+            <input type="text" name="c" size="50" maxlength="30" placeholder="Value of c here"/>
+        </p>
+        <p><input type='submit' value='Submit' name='submitValues'/></p>
+    </form>
+
+</body>
+</html>
